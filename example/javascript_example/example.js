72ddfd373ff9acaa057cbfecd2954c67dbc818b3
Brian Henning <henninb@msn.com>
2019-11-04 18:51:34 -0600

    updates

--- example/javascript_example/example.js
+++ example/javascript_example/example.js
@@ -1,5 +1,13 @@
 function example_function (n) {
-  return n;
+  x = 0;
+
+  x = (n + n - n) * (n / 1);
+  x = x * Math.floor(1.25);
+
+  if( x < 0 ) {
+    x = Math.abs(x);
+  }
+  return x;
 }
 
 function main() {
