0d09ccf07434cc5e9fee7e05db4d84882ca8acde
Brian Henning <henninb@msn.com>
2019-11-08 08:24:24 -0600

    updates

---
+++ lambda_functions/kotlin_lambda_functions/LambdaFunctions.kt
@@ -0,0 +1,28 @@
+package LambdaFunctions
+
+class LambdaFunctions {
+  val calculateGrade = { grade : Int ->
+    when(grade) {
+        in 0..40 -> "Fail"
+        in 41..70 -> "Pass"
+        in 71..100 -> "Distinction"
+        else -> "unknown"
+    }
+  }
+
+  val square = { number: Int -> number * number }
+}
+
+fun main(args : Array<String>) {
+  if( args.size != 0 ) {
+    println("Usage: java -jar LambdaFunctions.jar <noargs>")
+    System.exit(1)
+  }
+
+  val lambda_functions = LambdaFunctions()
+  for (idx_i in 1..9) {
+    println("lambda_function(" + idx_i + ")=<" + lambda_functions.calculateGrade(idx_i) + ">")
+    println("lambda_function(" + idx_i + ")=<" + lambda_functions.square(idx_i) + ">")
+  }
+}
+
