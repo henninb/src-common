3e5740e89ba8148feb1b3d35e7156d7dc16b55e3
Brian Henning <henninb@msn.com>
2019-12-03 20:33:48 -0600

    fibonacci in haskell.

--- fibonacci/haskell_fibonacci/fibonacci.hs
+++ fibonacci/haskell_fibonacci/fibonacci.hs
@@ -1,19 +1,28 @@
---comment
-
 --fibonacci n = if n == 0 then 1 else (fibonacci( n - 1 ) + fibonacci( n - 2 ))
 
-fibonacci::Int->Int
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = fibonacci(n-1) + fibonacci(n-2)
+-- fibonacciOne::Int->Int
+fibonacciOne 0 = 0
+fibonacciOne 1 = 1
+fibonacciOne n = fibonacciOne(n-1) + fibonacciOne(n-2)
 
-repeatNTimes 0  = return ()
-repeatNTimes idx_i  =
- do
-  repeatNTimes (idx_i-1)
-  --putStrLn("fibonacci(" ++ show(idx_i) ++ ")=<" ++ show(fib(idx_i)) ++ ">")
-  putStrLn("fibonacci(" ++ show(idx_i) ++ ")=<" ++ show(fibonacci(idx_i)) ++ ">")
+--repeatNTimes 0  = return ()
+--repeatNTimes idx_i  =
+-- do
+--  repeatNTimes (idx_i-1)
+--  --putStrLn("fibonacci(" ++ show(idx_i) ++ ")=<" ++ show(fib(idx_i)) ++ ">")
+--  putStrLn("fibonacci(" ++ show(idx_i) ++ ")=<" ++ show(fibonacci(idx_i)) ++ ">")
 
-main = do
-   --args <- getArgs
-   repeatNTimes(24)
+--fibonacci = map fibonacciOne
+--fibonacci :: (Show a, Integral a) => [a] -> [String]
+fibonacci [] = []
+fibonacci (x:xs) = fibonacciOne x : fibonacci xs
+
+-- need to fix the main
+-- main :: IO()
+-- main = putStrLn $ fibonacci [1..100]
+-- main = map (putStrLn . show) [1,2,3,4]
+-- main =  putStrLn $ show [1..100]
+
+--main = do
+--   --args <- getArgs
+--   repeatNTimes(24)
