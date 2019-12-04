701ac82faf50ae23e1fbe0ebeeccbb050b5fe1a6
Brian Henning <henninb@msn.com>
2019-12-03 19:48:56 -0600

    haskell example updates

--- example/haskell_example/example.hs
+++ example/haskell_example/example.hs
@@ -6,15 +6,18 @@ import Data.Char
 import Data.Time
 --import System
 
-example_function n = n
+example_function_one n = show n
 
-repeatNTimes 0  = return ()
-repeatNTimes idx_i  =
- do
-  repeatNTimes (idx_i-1)
-  putStrLn("example_function(" ++ show(idx_i) ++ ")=<" ++ show(example_function(idx_i)) ++ ">")
+example_function = map example_function_one
+-- repeatNTimes 0  = return ()
+-- repeatNTimes idx_i  =
+--  do
+--   repeatNTimes (idx_i-1)
+--   putStrLn("example_function(" ++ show(idx_i) ++ ")=<" ++ show(example_function(idx_i)) ++ ">")
 
-main = do
-   --args <- getArgs
-  --getArgs >>= print
-  repeatNTimes(9)
+--main = do
+--   --args <- getArgs
+--  --getArgs >>= print
+--  repeatNTimes(9)
+
+main = putStrLn $ unlines $ example_function [1..10]
