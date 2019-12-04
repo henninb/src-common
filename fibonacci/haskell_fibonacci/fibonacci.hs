f74d83c8bac20279acbda27a7d14ee0b270eefcc
Brian Henning <henninb@msn.com>
2019-12-03 20:46:16 -0600

    updates to fib

--- fibonacci/haskell_fibonacci/fibonacci.hs
+++ fibonacci/haskell_fibonacci/fibonacci.hs
@@ -1,6 +1,8 @@
 --fibonacci n = if n == 0 then 1 else (fibonacci( n - 1 ) + fibonacci( n - 2 ))
 
+-- :type fibonacciOne
 -- fibonacciOne::Int->Int
+fibonacciOne :: (Num a1, Num a2) => a2 -> a1
 fibonacciOne 0 = 0
 fibonacciOne 1 = 1
 fibonacciOne n = fibonacciOne(n-1) + fibonacciOne(n-2)
@@ -14,11 +16,14 @@ fibonacciOne n = fibonacciOne(n-1) + fibonacciOne(n-2)
 
 --fibonacci = map fibonacciOne
 --fibonacci :: (Show a, Integral a) => [a] -> [String]
+fibonacci :: (Num a, Num a2) => [a2] -> [a]
 fibonacci [] = []
 fibonacci (x:xs) = fibonacciOne x : fibonacci xs
 
 -- need to fix the main
--- main :: IO()
+main :: IO()
+-- main = putStrLn "fibonacci"
+main = putStrLn $ unlines $ fibonacci [1..100]
 -- main = putStrLn $ fibonacci [1..100]
 -- main = map (putStrLn . show) [1,2,3,4]
 -- main =  putStrLn $ show [1..100]
