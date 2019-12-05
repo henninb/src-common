988acca263c407ad51bcc57590129c30e8df0f93
Brian Henning <henninb@msn.com>
2019-12-04 20:06:42 -0600

    updates factorial haskell

--- factorial/haskell_factorial/factorial.hs
+++ factorial/haskell_factorial/factorial.hs
@@ -1,15 +1,12 @@
 --comment
 --factorial n = if n == 0 then 1 else n * factorial (n-1)
-factorial::Int->Int
+factorial:: Int -> Int
 factorial 0 = 0
 factorial 1 = 1
 factorial n = n * factorial(n-1)
 
-repeatNTimes 0  = return ()
-repeatNTimes idx_i  =
- do
-  repeatNTimes (idx_i-1)
-  putStrLn("factorial(" ++ show(idx_i) ++ ")=<" ++ show(factorial(idx_i)) ++ ">")
+factorialList :: [Int] -> String
+factorialList = unlines . map (show . factorial)
 
-main =
-  repeatNTimes(12)
+main :: IO()
+main = putStrLn $ factorialList [0..20]
