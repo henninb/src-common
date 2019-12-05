34f70b036275c864b80a862bc79f490e9e3009bb
Brian Henning <henninb@msn.com>
2019-12-04 20:34:10 -0600

    updates to example haskell

--- example/haskell_example/example.hs
+++ example/haskell_example/example.hs
@@ -1,23 +1,9 @@
 --comment
-import Text.Printf
---import Text.Format
-import Data.Char
---import Time
-import Data.Time
---import System
 
-example_function_one n = show n
+example_function :: Int -> Int
+example_function n = (n + n - n) * n
 
-example_function = map example_function_one
--- repeatNTimes 0  = return ()
--- repeatNTimes idx_i  =
---  do
---   repeatNTimes (idx_i-1)
---   putStrLn("example_function(" ++ show(idx_i) ++ ")=<" ++ show(example_function(idx_i)) ++ ">")
+exampleList = unlines . map (show . example_function)
 
---main = do
---   --args <- getArgs
---  --getArgs >>= print
---  repeatNTimes(9)
-
-main = putStrLn $ unlines $ example_function [1..10]
+main :: IO()
+main = putStrLn $ exampleList [1..10]
