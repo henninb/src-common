e09ef091f1caa6f7102c569855a9247199874180
Brian Henning <henninb@msn.com>
2020-10-26 07:50:29 -0500

    updates

--- easter_calculator/haskell_easter_calculator/easter_calculator.hs
+++ easter_calculator/haskell_easter_calculator/easter_calculator.hs
@@ -1,8 +1,11 @@
 main :: IO ()
 main = do
   print (calculateEasterSunday 2020)
+  print (easter 2020)
   print (calculateEasterSunday 2021)
+  print (easter 2021)
   print (calculateEasterSunday 2022)
+  print (easter 2022)
 
 calculateEasterSunday :: Integer -> String
 calculateEasterSunday year = show year ++ "-" ++ show month ++ "-" ++ show day
@@ -16,3 +19,14 @@ calculateEasterSunday year = show year ++ "-" ++ show month ++ "-" ++ show day
     x = ((70004 - d - p) `mod` 7) + 1
     month = if (p + x) < 11 then 3 else 4
     day = if (p + x) < 11 then ((p + x) + 21) else ((p + x) - 10)
+
+
+easter year = show year ++ "-" ++ show month ++ "-" ++ show newday
+  where
+    g = year `mod` 19
+    c = year `div` 100
+    h = (c - (c `div` 4) - ((8 * c + 13) `div` 25) + 19 * g + 15) `mod` 30
+    i = h - (h `div` 28) * (1 - (h `div` 28) * (29 `div` (h + 1)) * ((21 - g) `div` 11))
+    day = i - ((year + (year `div` 4) + i + 2 - c + (c `div` 4)) `mod` 7) + 28
+    month = if day > 31 then 4 else 3
+    newday = if day > 31 then day-31 else day
